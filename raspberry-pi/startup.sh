f289c68e54d9cb59a942b5f32b0af6c466e60a5a
Alan Palazzolo <github@zzolo.org>
2015-11-14 12:09:01 -0600

    Updating raspberry pi code for new name.

--- raspberry-pi/startup.sh
+++ raspberry-pi/startup.sh
@@ -4,7 +4,7 @@
 sudo /home/pi/installed/Adafruit-Retrogame/retrogame &
 
 # Go to project
-cd /home/pi/installed/pickle-jumper/;
+cd /home/pi/installed/determined-dill/;
 
 # Check if internet is connected
 wget -q --tries=10 --timeout=20 --spider http://google.com > /dev/null
@@ -20,4 +20,4 @@ forever start ./node_modules/http-server/bin/http-server $(pwd) -p 8080;
 sleep 5;
 
 # Start chromium
-chromium --kiosk --ignore-certificate-errors http://127.0.0.1:8080/;
+chromium --kiosk --ignore-certificate-errors --restore-last-session http://127.0.0.1:8080/;
